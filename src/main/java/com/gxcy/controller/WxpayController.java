2f718861d818d562000efd04d56255c39cda453f
ssx <1779876935@qq.com>
2020-12-06 00:34:15 +0800

    wxpayController

---
+++ src/main/java/com/gxcy/controller/WxpayController.java
@@ -0,0 +1,9 @@
+package com.gxcy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/wxpay")
+public class WxpayController {
+}
