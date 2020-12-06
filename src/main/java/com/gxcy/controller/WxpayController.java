463dfbdbfd79699bd1dd83a0685a1daeb31f0cd6
ssx <1779876935@qq.com>
2020-12-06 12:00:22 +0800

    授权

--- src/main/java/com/gxcy/controller/WxpayController.java
+++ src/main/java/com/gxcy/controller/WxpayController.java
@@ -1,9 +1,119 @@
 package com.gxcy.controller;
 
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.gxcy.utils.AesCbcUtil;
+import com.gxcy.utils.HttpRequestUtil;
+import com.gxcy.utils.WeChatUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.xml.transform.Result;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/wxpay")
 public class WxpayController {
+    private static final long serialVersionUID = 1L;
+    private static Logger logger = LoggerFactory.getLogger(WxpayController.class);
+
+    /**
+     * 登录
+     * @param
+     */
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @ResponseBody
+    @GetMapping(value="/login")
+    public Object login(String encryptedData, String iv,String code) {
+        logger.info("开始验证");
+        // 微信小程序ID
+        String appid = "wx7d7900c4f6e6f09c";
+        // 微信小程序秘钥
+        String secret = "3bf4648c1554684716abf617243b9e63";
+/*
+
+        // 根据小程序穿过来的code想这个url发送请求
+        String url = "https://api.weixin.qq.com/sns/jscode2session?appid=" + appid + "&secret=" + secret + "&js_code=" + code + "&grant_type=authorization_code";
+        // 发送请求，返回Json字符串
+        String str = WeChatUtil.httpRequest(url, "GET", null);
+        // 转成Json对象 获取openid
+        JSONObject jsonObject = JSONObject.parseObject(str);
+
+        // 我们需要的openid，在一个小程序中，openid是唯一的
+        String openid = jsonObject.get("openid").toString();
+        System.out.println("openid："+openid);
+
+        // 然后书写自己的处理逻辑即可
+*/
+
+
+        Map map = new HashMap();
+
+        // 登录凭证不能为空
+        if (code == null || code.length() == 0) {
+            map.put("status", 0);
+            map.put("msg", "code 不能为空");
+            return map;
+        }
+
+        // 小程序唯一标识 (在微信小程序管理后台获取)
+        String wxspAppid = "wx7d7900c4f6e6f09c";
+        // 小程序的 app secret (在微信小程序管理后台获取)
+        String wxspSecret = "3bf4648c1554684716abf617243b9e63";
+        // 授权（必填）
+        String grant_type = "authorization_code";
+
+      //  1、向微信服务器 使用登录凭证 code 获取 session_key 和 openid
+
+        // 请求参数
+        String params = "appid=" + wxspAppid + "&secret=" + wxspSecret + "&js_code=" + code + "&grant_type="
+                + grant_type;
+        logger.info("请求参数:"+params);
+        // 发送请求
+        String sr = HttpRequestUtil.sendGet("https://api.weixin.qq.com/sns/jscode2session", params);
+        // 解析相应内容（转换成json对象）
+        JSONObject json = JSON.parseObject(sr);
+        logger.info("结果："+json);
+        // 获取会话密钥（session_key）
+        String session_key = json.get("session_key").toString();
+        // 用户的唯一标识（openid）
+        String openid = (String) json.get("openid");
+
+       // 2、对encryptedData加密数据进行AES解密
+        try {
+            String result = AesCbcUtil.decrypt(encryptedData, session_key, iv, "UTF-8");
+            if (null != result && result.length() > 0) {
+                map.put("status", 1);
+                map.put("msg", "解密成功");
+
+                JSONObject userInfoJSON = JSON.parseObject(result);
+                Map userInfo = new HashMap();
+                userInfo.put("openId", userInfoJSON.get("openId"));
+                userInfo.put("nickName", userInfoJSON.get("nickName"));
+                userInfo.put("gender", userInfoJSON.get("gender"));
+                userInfo.put("city", userInfoJSON.get("city"));
+                userInfo.put("province", userInfoJSON.get("province"));
+                userInfo.put("country", userInfoJSON.get("country"));
+                userInfo.put("avatarUrl", userInfoJSON.get("avatarUrl"));
+                // 解密unionId & openId;
+
+                userInfo.put("unionId", userInfoJSON.get("unionId"));
+                map.put("userInfo", userInfo);
+            } else {
+                map.put("status", 0);
+                map.put("msg", "解密失败");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+
 }
