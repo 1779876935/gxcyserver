586a48cab0dc35d68f7908d4807d8a86c8d7e516
ssx <1779876935@qq.com>
2020-12-07 20:14:53 +0800

    设备controller挡板

--- src/main/java/com/gxcy/controller/EquipmentController.java
+++ src/main/java/com/gxcy/controller/EquipmentController.java
@@ -5,16 +5,27 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/eqment")
 public class EquipmentController {
     private static Logger logger = LoggerFactory.getLogger(EquipmentController.class);
 
     @RequestMapping("/queryEquipment")
-    public Object queryEquipment(String  openID, String longitude,String dimension){
-        logger.info("开始获取当前位置的车衣{},{},{}",openID,longitude,dimension);
-
-        return null;
+    public Object queryEquipment( String longitude,String dimension){
+        logger.info("开始获取当前位置的车衣经度{},纬度{}",longitude,dimension);
+        List<Map<String,String>> eqMents = new ArrayList<>();
+        for (int i = 0 ; i<10 ;i++){
+            Map<String,String> map = new HashMap<>();
+            map.put("longitude",String.valueOf(Integer.parseInt(longitude)+i));
+            map.put("dimension",String.valueOf(Integer.parseInt(dimension)+i));
+            eqMents.add(map);
+        }
+         return eqMents;
     }
 
 }
